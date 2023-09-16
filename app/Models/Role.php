9883354377cd65440008fabf52b042a3b95edb65
hubert <hubert.zajac@allboards.eu>
2023-09-16 17:03:21 +0200

    PA-1: Create user, role and permission resource

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
@@ -13,4 +14,9 @@ class Role extends SpatieRole
 
     protected $keyType = 'string';
     public $incrementing = false;
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
 }
