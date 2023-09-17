e800789525bff331b701423e883994d9b43b3648
hubert <hubert.zajac@allboards.eu>
2023-09-17 09:03:38 +0200

    PA-1: Create product service

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,16 +6,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProductResource;
 use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Services\Contracts\ProductServiceInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
     private ProductRepositoryInterface $productRepository;
+    private ProductServiceInterface $productService;
 
-    public function __construct(ProductRepositoryInterface $productRepository)
+    public function __construct(ProductRepositoryInterface $productRepository, ProductServiceInterface $productService)
     {
         $this->productRepository = $productRepository;
+        $this->productService = $productService;
     }
 
     public function index(): ResourceCollection
@@ -26,5 +30,23 @@ class ProductController extends Controller
     public function show(string $id): ProductResource
     {
         return $this->productRepository->show($id);
+    }
+
+    public function store(Request $request): Response
+    {
+        $this->authorize('edit', 'products');
+        return $this->productService->store($request);
+    }
+
+    public function update(string $id, Request $request): Response
+    {
+        $this->authorize('edit', 'products');
+        return $this->productService->update($id, $request);
+    }
+
+    public function destroy(string $id): Response
+    {
+        $this->authorize('edit', 'products');
+        return $this->productService->destroy($id);
     }
 }
--- app/Providers/ProductServiceProvider.php
+++ app/Providers/ProductServiceProvider.php
@@ -6,6 +6,8 @@ namespace App\Providers;
 
 use App\Repositories\Contracts\ProductRepositoryInterface;
 use App\Repositories\ProductRepository;
+use App\Services\Contracts\ProductServiceInterface;
+use App\Services\ProductService;
 use Illuminate\Support\ServiceProvider;
 
 class ProductServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class ProductServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(ProductServiceInterface::class, ProductService::class);
     }
 
     /**
